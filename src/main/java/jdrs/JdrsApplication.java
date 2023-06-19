e4409aa5a2ed53d6a5edbd413a185485db2de66a
Seth Hillsberry <Shillsberry@protonmail.com>
2023-06-19 11:45:00 -0500

    Initializing

---
+++ src/main/java/jdrs/JdrsApplication.java
@@ -0,0 +1,13 @@
+package jdrs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JdrsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JdrsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/jdrs/JdrsApplicationTests.java
@@ -0,0 +1,13 @@
+package jdrs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JdrsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
